bd4973e5ab46d80d44f61873392920c7dbc8e833
Marcus Vinicius <marcus.silva@maps.com.br>
2021-07-25 20:06:02 -0300

    primeiro project

---
+++ src/main/java/br/com/udemy/couse_secao25/CourseSecao25Application.java
@@ -0,0 +1,13 @@
+package br.com.udemy.couse_secao25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSecao25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSecao25Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/udemy/couse_secao25/CourseSecao25ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udemy.couse_secao25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSecao25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
